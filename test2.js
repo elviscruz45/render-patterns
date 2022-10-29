6edb8a6a65be84b529f0bca9e1e8a4d9fe203f26
Elvis <elviscruz45@gmail.com>
2022-10-29 11:11:01 -0500

    end

--- test2.js
+++ test2.js
@@ -1,3 +1,5 @@
-a={hola:"perro",companero: "senor godos"}
-
-console.log({...a})
+a={isTrusted: true, 
+    key: 'TODOS_V1', 
+    oldValue: '[{"completed":true,"text":"Cocinar para el almuerzo"},{"completed":false,"text":"saludar"}]', newValue: '[{"completed":true,"text":"Cocinar para el almuerz…t":"saludar"},{"completed":false,"text":"holas"}]',
+    url: 'http://localhost:3000/'
+}
